a9dab00b5699f592826c1087e160a808653d3529
EC2 Default User <ec2-user@ip-172-31-20-198.us-east-2.compute.internal>
2021-03-18 05:16:33 +0000

    commit

---
+++ dist/_nuxt/static/1616042979/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616042979",layout:"default",error:a,serverRendered:true,routePath:b,config:{apiKey:c,baseUrl:c,app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2c143086"}},globalRefs:{}}}(null,"\u002F",void 0));
