1e97debbe104996a28542a52e53cb1f2ee6129de
EC2 Default User <ec2-user@ip-172-31-20-198.us-east-2.compute.internal>
2021-05-20 06:36:39 +0000

    loading animation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="flex flex-col h-screen justify-between">
+    <div id="loading">
+      <div id="loader">
+  <div id="shadow"></div>
+  <div id="box"></div>
+</div>
+    </div>
     <transition
       name="headerNav"
     >
@@ -38,6 +44,88 @@
   a{
     -webkit-tap-highlight-color: rgba(0,0,0,0);
   }
+  /* ローディング画面 */
+#loading {
+  width: 100vw;
+  height: 100vh;
+  transition: all 1s;
+  background-color: #fca45c;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 9999;
+}
+.spinner {
+  width: 100px;
+  height: 100px;
+  margin: 200px auto;
+  background-color: #fff;
+  border-radius: 100%;
+  animation: sk-scaleout 1.0s infinite ease-in-out;
+}
+/* ローディングアニメーション */
+@keyframes sk-scaleout {
+  0% {
+    transform: scale(0);
+  } 100% {
+    transform: scale(1.0);
+    opacity: 0;
+  }
+}
+.loaded {
+  opacity: 0;
+  visibility: hidden;
+}
+#loader {
+  /* Uncomment this to make it run! */
+  /*
+     animation: loader 5s linear infinite; 
+  */
+  
+  position: absolute;
+  top: calc(50% - 20px);
+  left: calc(50% - 20px);
+}
+@keyframes loader {
+  0% { left: -100px }
+  100% { left: 110%; }
+}
+#box {
+  width: 50px;
+  height: 50px;
+  background: #fff;
+  animation: animate .5s linear infinite;
+  position: absolute;
+  top: 0;
+  left: 0;
+  border-radius: 3px;
+}
+@keyframes animate {
+  17% { border-bottom-right-radius: 3px; }
+  25% { transform: translateY(9px) rotate(22.5deg); }
+  50% {
+    transform: translateY(18px) scale(1,.9) rotate(45deg) ;
+    border-bottom-right-radius: 40px;
+  }
+  75% { transform: translateY(9px) rotate(67.5deg); }
+  100% { transform: translateY(0) rotate(90deg); }
+} 
+#shadow { 
+  width: 50px;
+  height: 5px;
+  background: #000;
+  opacity: 0.1;
+  position: absolute;
+  top: 59px;
+  left: 0;
+  border-radius: 50%;
+  animation: shadow .5s linear infinite;
+}
+@keyframes shadow {
+  50% {
+    transform: scale(1.2,1);
+  }
+}
 </style>
 <script>
 import Header from '@/components/Header'
@@ -64,6 +152,10 @@ export default {
     window.addEventListener('load', () => {
       this.onScroll()
     })
+    window.onload = function() {
+      const spinner = document.getElementById('loading');
+      spinner.classList.add('loaded');
+    }
   },
   watch: {
     // 上にスクロールした時に表示
--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,6 @@ export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
   ssr: 'true',
-
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'Monotone',
