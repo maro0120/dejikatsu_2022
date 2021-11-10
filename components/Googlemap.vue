a5dbee0e3e093e8446a516b7fdf94289e96a110b
EC2 Default User <ec2-user@ip-172-31-1-171.ap-northeast-1.compute.internal>
2021-11-10 02:58:09 +0000

    loading=lazy

--- components/Googlemap.vue
+++ components/Googlemap.vue
@@ -14,15 +14,15 @@
     <div class="text-center" >
     <h2 class="text-xl text-gray-800">弓削会場</h2>
     <div class="gmap mb-5">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3271.0620403274997!2d133.95894461523847!3d34.92998098037621!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x35543cdf8f0442f3%3A0x143f330793f15a51!2z5LmF57Gz5Y2X55S65paH5YyW44K744Oz44K_44O8!5e0!3m2!1sja!2sjp!4v1634107215733!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+      <iframe loading="lazy" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3271.0620403274997!2d133.95894461523847!3d34.92998098037621!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x35543cdf8f0442f3%3A0x143f330793f15a51!2z5LmF57Gz5Y2X55S65paH5YyW44K744Oz44K_44O8!5e0!3m2!1sja!2sjp!4v1634107215733!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </div>
     <h2 class="text-xl text-gray-800">神目会場</h2>
     <div class="gmap mb-5">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3272.4102679499274!2d133.92974895065154!3d34.89615238013682!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x355418105385f113%3A0x6bd18dbddc663d59!2z5LmF57Gz5Y2X55S6IOWFrOawkemkqOelnuebruaUr-mkqA!5e0!3m2!1sja!2sjp!4v1634107304395!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+      <iframe loading="lazy" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3272.4102679499274!2d133.92974895065154!3d34.89615238013682!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x355418105385f113%3A0x6bd18dbddc663d59!2z5LmF57Gz5Y2X55S6IOWFrOawkemkqOelnuebruaUr-mkqA!5e0!3m2!1sja!2sjp!4v1634107304395!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </div>
     <h2 class="text-xl text-gray-800">誕生寺会場</h2>
     <div class="gmap mb-5">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3270.132428390251!2d133.95422295065305!3d34.95328937705994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x35543cb96f4ae025%3A0xdc55e726936c5703!2z5LmF57Gz5Y2X55S6IOWFrOawkemkqOiqleeUn-WvuuaUr-mkqA!5e0!3m2!1sja!2sjp!4v1634107349090!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+      <iframe loading="lazy" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3270.132428390251!2d133.95422295065305!3d34.95328937705994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x35543cb96f4ae025%3A0xdc55e726936c5703!2z5LmF57Gz5Y2X55S6IOWFrOawkemkqOiqleeUn-WvuuaUr-mkqA!5e0!3m2!1sja!2sjp!4v1634107349090!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </div>
     </div>
   </div>
